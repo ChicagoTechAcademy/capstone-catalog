cb83b0fef7379b4c696512d269376b958c73e235
Ellen Gonzalez <egonzalez@chicagotechacademy.org>
2016-01-14 16:30:54 +0000

    worked on feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112154804) do
+ActiveRecord::Schema.define(version: 20160114154437) do
 
   create_table "teams", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "focus_room_number"
   end
 
 end
